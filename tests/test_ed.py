11d5813a66243c17e6c58d2432abefe2578519b0
Tim Butters <tim.d.butters@gmail.com>
2024-10-21 23:16:11 +0100

    Working basic read test

--- tests/test_ed.py
+++ tests/test_ed.py
@@ -1,6 +1,10 @@
+import pandas as pd
+
 from ed import Ed
 
 
 def test_db_create():
-    db = Ed("testdb")
-    db.sql("SELECT * FROM df")
+    db = Ed("tests/testdb")
+    res = db.sql("SELECT * FROM db")
+    assert isinstance(res, pd.DataFrame)
+    assert len(res) == 2
