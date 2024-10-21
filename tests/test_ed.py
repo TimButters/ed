ef497259675978984ab5a47bc4a9363eead5767c
Tim Butters <tim.d.butters@gmail.com>
2024-10-21 22:41:43 +0100

    Update test

--- tests/test_ed.py
+++ tests/test_ed.py
@@ -1,6 +1,6 @@
-from ed import Ed, sql
+from ed import Ed
 
 
 def test_db_create():
-    db = EdDb("./test.xlsx")
-    sql("SELECT * FROM df")
+    db = Ed("testdb")
+    db.sql("SELECT * FROM df")
