04c208db478b830b4084a5a3f230ea10fd4fa2b2
Tim Butters <tim.d.butters@gmail.com>
2024-10-21 23:16:33 +0100

    Db is a directory of Excel files

--- src/ed/db.py
+++ src/ed/db.py
@@ -2,12 +2,18 @@ import pandas as pd
 import pandasql as ps
 import re
 
+from pathlib import Path
+
 
 class Ed:
-    def __init__(self, filename):
-        self._df = pd.read_excel(filename)
+    def __init__(self, dbname):
+        dfs = []
+        for f in Path(dbname).iterdir():
+            if f.match("*.xlsx"):
+                dfs.append(pd.read_excel(f))
+        self._df = pd.concat(dfs)
 
     def sql(self, query: str):
         df = self._df
         q = re.sub(r"FROM\s+\w+", r"FROM df", query, re.IGNORECASE)
-        return ps.sqldf(q, globals())
+        return ps.sqldf(q, locals())
